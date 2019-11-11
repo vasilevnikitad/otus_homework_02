0195bf628b2d676c62517d8565f6d1cc6325dc10
Nikita Vasilev <vasilevnikitad@gmail.com>
2019-11-11 13:20:29 +0300

    finish trivial test

--- tst/trivial.cpp
+++ tst/trivial.cpp
@@ -4,8 +4,9 @@
 
 #include <boost/test/unit_test.hpp>
 
-#include <fstream>
+#include <algorithm>
 #include <exception>
+#include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -13,25 +14,35 @@
 BOOST_AUTO_TEST_SUITE(trivial)
 
 BOOST_AUTO_TEST_CASE(trivial_data) try {
-  std::ifstream is{"trivial_data_input.txt"};
+  std::ifstream data_stream{"trivial_data_input.txt"};
 
-  if (!is)
+  if (!data_stream)
     throw std::ios_base::failure{"Cannot open file for reading"};
 
-  std::stringstream ss;
+  std::stringstream filter_data_stream;
 
   ip_filter::filter_pipe(
-      is,
-      ss,
+      data_stream,
+      filter_data_stream,
       [](ip_filter::ip const &)  {return true;},
       [](ip_filter::ip const &ip){return ip[0] == 1;},
       [](ip_filter::ip const &ip){return ip[0] == 46 && ip[1] == 70;},
       [](ip_filter::ip const &ip){return std::any_of(std::begin(ip),
                                                      std::end(ip),
                                                      [](ip_filter::octet_t v){return v == 46;});}
-      );
+  );
 
-  //TODO: comparsion
+  std::ifstream expect_stream{"trivial_data_expectation.txt"};
+  if (!expect_stream)
+    throw std::ios_base::failure{"Cannot open file for reading"};
+
+  BOOST_CHECK(
+      std::equal(
+        std::istream_iterator<std::string>(expect_stream),
+        std::istream_iterator<std::string>(),
+        std::istream_iterator<std::string>(filter_data_stream)
+      )
+  );
 } catch(std::ios_base::failure const& e) {
   BOOST_FAIL(e.what());
 } catch(std::exception const &e) {
